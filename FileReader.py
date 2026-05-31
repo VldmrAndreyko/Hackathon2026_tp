21829a1c91975e8884f11cedf556c225c30fba87
Андрейко Владимир <vlmiandreyko@edu.hse.ru>
2026-05-31 15:50:41 +0300

    Добавлена проверка расширения файла, исправленны сообщения ошибок и прерывание цикла в read_directory

--- FileReader.py
+++ FileReader.py
@@ -6,24 +6,29 @@ from Email import Email
 class FileReader:
     def __init__(self):
         self.stats = {'files_read': 0, 'files_failed': 0}
+        self.extension = ".txt"
 
     def read_file(self, file_path: str):
         path = Path(file_path)
 
         if not path.exists():
             self.stats['files_failed'] += 1
-            raise ValueError("Указан неверный путь к файлу")
+            raise ValueError(f"Указан неверный путь к файлу {path}")
 
         if not path.is_file():
             self.stats['files_failed'] += 1
-            raise ValueError("По данному пути находится не файл")
+            raise ValueError(f"По данному пути находится не файл {path}")
+
+        if path.suffix != self.extension:
+            self.stats['files_failed'] += 1
+            raise ValueError(f"Расширение данного файла не поддерживается {path}")
 
         try:
             with open (path, 'r') as file:
                 text = file.read()
-                if text is None:
+                if text.strip() == "":
                     self.stats['files_failed'] += 1
-                    raise ValueError("Текст в файле не может быть равен None")
+                    raise ValueError(f"Файл пустой {path}")
 
                 lines = text.splitlines()
                 email = Email(lines)
@@ -39,17 +44,22 @@ class FileReader:
 
         if not path.exists():
             self.stats['files_failed'] += 1
-            raise ValueError("Указан неверный путь к директории")
+            raise ValueError(f"Указан неверный путь к директории {path}")
 
         if not path.is_dir():
             self.stats['files_failed'] += 1
-            raise ValueError("По данному пути находится не директория")
+            raise ValueError(f"По данному пути находится не директория {path}")
 
 
         emails = []
         for file_path in path.glob("*"):
-            email = self.read_file(file_path)
-            emails.append(email)
+            if file_path.is_dir():
+                continue
+            try:
+                email = self.read_file(file_path)
+                emails.append(email)
+            except Exception as error:
+                print(f"Ошибка чтения файла {file_path}: {error}")
 
         return emails
 
