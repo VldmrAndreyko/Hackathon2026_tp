4c26012f39957beb365a69657788016a98e11d67
Андрейко Владимир <vlmiandreyko@edu.hse.ru>
2026-05-31 15:06:25 +0300

    Начальная реализация класса FileReader: метод read_file получает на вход путь к файлу и возвращает экземпляр класса Email

---
+++ FileReader.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+from Email import Email
+
+
+class FileReader:
+    def __init__(self):
+        self.stats = {'files_read': 0, 'files_failed': 0}
+
+    def read_file(self, file_path: str):
+        path = Path(file_path)
+
+        if not path.exists() or not path.is_file():
+            self.stats['files_failed'] += 1
+            raise ValueError("Указан неверный путь к файлу")
+
+        if not path.is_file():
+            self.stats['files_failed'] += 1
+            raise ValueError("По данному пути находится не файл")
+
+        try:
+            with open (path, 'r') as file:
+                text = file.read()
+                if text is None:
+                    self.stats['files_failed'] += 1
+                    raise ValueError("Текст в файле не может быть равен None")
+
+                lines = text.splitlines()
+                email = Email(lines)
+
+                self.stats['files_read'] += 1
+                return email
+        except Exception as error:
+            self.stats['files_failed'] += 1
+            raise error
+
+
+
+
+
+
